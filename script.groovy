2a0351aafa6ce7c53bc2c1ea866ce4bbe92dd473
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2024-03-12 15:15:26 +0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,10 +2,10 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
 
 void function() {
-  writeFile file: 'groovy.txt', text: env.TARGET_BRANCH
-  def line = env.TARGET_BRANCH
-  echo line
-  assert line.toString().contains('merge dev to main')
+  writeFile file: 'groovy.txt', text: env.GIT_LOG
+  String git_log = env.GIT_LOG
+  String sub_msg = "refs/remotes/origin/main"
+  assertThat(git_log, containsString(sub_msg));
 }
 
 return this
